91ecbd660675b841bfccfb3699799789cd324324
Dave Aitken <dave.aitken@gmail.com>
2026-01-19 12:57:58 +0000

    refactor: extract window options setup to shared utility
    Add setup_view_window_options() to lua/gitlad/utils/init.lua for
    setting standard window-local options (number, relativenumber,
    signcolumn, foldcolumn, wrap) used by buffer views.
    
    Both status.lua and log.lua had identical 5-line blocks setting
    these options. Now they call the shared utility function.

--- lua/gitlad/utils/init.lua
+++ lua/gitlad/utils/init.lua
@@ -1,11 +1,22 @@
 ---@mod gitlad.utils Utility modules
 ---@brief [[
---- Index for utility modules.
+--- Index for utility modules and common utilities.
 ---@brief ]]
 
 local M = {}
 
 M.errors = require("gitlad.utils.errors")
 M.keymap = require("gitlad.utils.keymap")
+
+--- Set up standard window-local options for gitlad buffer views
+--- Used by status, log, and other buffer views for consistent display
+---@param winnr number Window number
+function M.setup_view_window_options(winnr)
+  vim.wo[winnr].number = false
+  vim.wo[winnr].relativenumber = false
+  vim.wo[winnr].signcolumn = "yes:1"
+  vim.wo[winnr].foldcolumn = "0"
+  vim.wo[winnr].wrap = false
+end
 
 return M
